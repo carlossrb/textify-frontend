e6a318f9832deae95fb92b1a5f831415900c55c0
Carlos <carlos.ribeiro.brito@hotmail.com>
2023-02-09 02:04:34 -0300

    feat: add new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
+        <img src={logo} className='App-logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
